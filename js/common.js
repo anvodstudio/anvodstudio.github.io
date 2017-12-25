f79ed39e33c773b8db547987192ed2ccdfc3505b
Artem Sheludko <sheludko.artem@gmail.com>
2017-12-25 23:01:11 +0200

    init project

---
+++ js/common.js
@@ -0,0 +1,6 @@
+---
+---
+{% include_relative scripts/jquery.min.js %}
+{% include_relative scripts/evil-icons.min.js %}
+{% include_relative scripts/jquery.vide.min.js %}
+{% include_relative scripts/main.js %}
